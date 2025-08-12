d6c0e79d3d76acc67425af3183c2ef080a745d24
João <joao.rodrigues0033@gmail.com>
2025-08-11 22:00:00 -0300

    add  and  files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,9 @@
-function status(request, response) {
-  //response.status(200).json({ chave: "Filipe é acima da media" });
+import database from "../../../../infra/database.js";
 
-  response.status(200).send("Sem Usar JSON");
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ chave: "Filipe é acima da media" });
 }
 
 export default status;
